ea44efb1b3db4e4af878bfd4e6fb10f61bdacdfe
takwa werfelli <takwa.werfelli@esprit.tn>
2025-06-07 20:53:30 +0100

    Initial commit - Flask app for Employee Attrition Prediction

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    input_array = np.array([data])
+    prediction = model.predict(input_array)
+
+    if prediction[0] == 1:
+        prediction_text = "️L'employé risque de quitter l'entreprise."
+    else:
+        prediction_text = "L'employé est susceptible de rester dans l'entreprise."
+
+    return render_template('index.html', prediction_text=prediction_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
